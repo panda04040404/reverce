6b61e31f36d76ec4d1b088ff6e83c044b53a9d30
panda04040404 <seanquial@gmail.com>
2025-06-12 22:47:22 +0900

    トレース

---
+++ style.css
@@ -0,0 +1,46 @@
+body {
+  margin: 8px;
+}
+
+table {
+  border: 3px outset gray;
+  background: #222;
+  box-sizing: border-box;
+  width: calc(100vw - 16px);
+  height: calc(100vw - 16px);
+  max-width: 400px;
+  max-height: 400px;
+}
+
+td {
+  background: green;
+  border: 0;
+  vertical-align: middle;
+}
+
+td p {
+  width: 90%;
+  height: 90%;
+  margin: auto;
+  border-radius: 50%;
+}
+
+.black p {
+  background-color: black;
+  box-shadow: 2px 2px 3px rgba(0, 0, 0, 0.3);
+}
+
+.white p {
+  background-color: white;
+  box-shadow: 2px -2px 3px rgba(0, 0, 0, 0.3);
+  transform: matrix(1, 0, 0, -1, 0, 0);
+}
+
+:root {
+  --transition-time: 400ms;
+}
+
+.reverse p {
+  transition: all var(--transition-time) cubic-bezier(0.25, 0.1, 0.25, 1),
+    background-color 0s calc(var(--transition-time) * 0.3);
+}
